d68e2eb73e6c92c7200ed1497f0f6ea4becb91da
codingHack <dbstjdwo1000@naver.com>
2018-06-23 12:42:07 +0900

    에러캐치 추가, 퍼그로 변경

--- app.js
+++ app.js
@@ -1,53 +1,70 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
+var passport = require('passport');
+//var passportConfig = require('./lib/passport-config');
+//var mysql = require('mysql'); 보고 추가 http://expressjs.com/ko/guide/database-integration.html#mysql
 
+/*-----------------url require-----------------*/ 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+/*-----------------url require-----------------*/ 
 var app = express();
 
-// view engine setup
+// view를 뭘로 쓸지 결정 - pug, html 등
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.locals.moment = require('moment');
+app.locals.querystring = require('querystring');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+
+// session을 사용할 수 있도록.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+app.use(flash()); // flash message를 사용할 수 있도록
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+/*-----------------url route-----------------*/
 app.use('/', index);
 app.use('/users', users);
+/*-----------------url route-----------------*/
 
-// catch 404 and forward to error handler
+
+
+// 에러처리
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error');//에러페이지 설정
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const catchErrors = require('../lib/async-error');
 
 /* GET home page. */
+router.get('/', catchErrors(async (req, res, next) => {
+  res.render('index');
+}));
+
 router.get('/', function(req, res, next) {
   res.sendFile(__dirname + "/main.html");
 });
-
-router.post('/email_post',function(req,res){
-  res.send("welcome "+req.body.email);
-  console.log(req.body.email);
-})
 
 module.exports = router;
