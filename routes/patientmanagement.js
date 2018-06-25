4a1e9291b50aaf922a4e40d9b2e3b74a49498f07
codingHack <dbstjdwo1000@naver.com>
2018-06-25 22:21:30 +0900

    patient 틀 작성

--- routes/patientmanagement.js
+++ routes/patientmanagement.js
@@ -4,7 +4,54 @@ const catchErrors = require('../lib/async-error');
 
 /* GET home page. */
 router.get('/', catchErrors(async (req, res, next) => {
-  res.render('patientmanagement/patientmain');
+  res.render('patientmanagement/main');
+}));
+
+/*환자 검색
+router.get('/', catchErrors(async (req, res, next) => {
+
+  res.render('');
+}));*/
+
+//---------------------환자 목록---------------------------------------------
+router.get('/list', catchErrors(async (req, res, next) => {
+  res.render('patientmanagement/list');
+}));
+
+//----------------------환자 상세정보-----------------------------------------
+router.get('/:id', catchErrors(async (req, res, next) => {
+  //환자상세정보 디비에서 가져옴
+  res.render('patientmanagement/show');
+}));
+
+//----------------------환자 추가----------------------------------------------
+router.get('/new', catchErrors(async (req, res, next) => {
+  res.render('patientmanagement/new');
+}));
+
+router.post('/new', catchErrors(async (req, res, next) => {
+  //디비에 저장
+  //저장 성공실패여부
+  res.render('patientmanagement/main');
+}));
+
+//------------------------환자 정보수정------------------------------------------
+router.get('/:id/edit', catchErrors(async (req, res, next) => {
+  //정보보여줌
+  res.render('patientmanagement/edit');
+}));
+
+router.put('/:id', catchErrors(async (req, res, next) => {
+  //id 값을 찾아서 업데이트
+  //저장 성공실패여부
+  res.render('patientmanagement/main');
+}));
+
+//---------------------------환자 삭제-------------------------------------------
+router.delete('/:id', catchErrors(async (req, res, next) => {
+  //id 값을 찾아서 ddelete
+  //저장 성공실패여부
+  res.render('patientmanagement/main');
 }));
 
 module.exports = router;
