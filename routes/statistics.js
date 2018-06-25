e24d812ee4bb63551c1b4d2906605ced2160c2d6
codingHack <dbstjdwo1000@naver.com>
2018-06-25 19:57:33 +0900

    statistic 분리

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function (req, res, next) {
-    res.render('employee/statistics');
+    res.render('statistics/statistics');
 });
 
 module.exports = router;
