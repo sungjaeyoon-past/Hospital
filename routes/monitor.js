fed4b87c30581d3ff466d84537c5dcc4b6cd8366
Nogadacoding <azxca1731@gamil.com>
2018-06-26 02:25:43 +0900

    monitor db값 받아오기

--- routes/monitor.js
+++ routes/monitor.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
 const catchErrors = require('../lib/async-error');
-
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: '58.123.136.107',
+  port: '3308',
+  user: 'web',
+  password: 'mju12345',
+  database: 'medic'
+});
 /* GET home page. */
 router.get('/', catchErrors(async (req, res, next) => {
-  res.render('employee/staticmain');
+  var personList = [];
+  conn.connect();
+  conn.query('SELECT * FROM patient', function (err, rows, fields) {
+    var person;
+    if (err)
+      console.log('Error while performing Query.', err);
+    else {
+      for (var i = 0; i < rows.length; i++) {
+        var person = {
+          'name': rows[i].name,
+          'personal_number': rows[i].personal_number,
+          'phone_number': rows[i].phone_number,
+          'gender': rows[i].gender,
+          'patient_id': rows[i].patient_id,
+        }
+        personList.push(person);
+      }
+      console.log('data is setting');
+      res.render('patientmanagement/monitor2', { "personList": personList });
+    }
+  });
+  conn.end();
 }));
 
 module.exports = router;
