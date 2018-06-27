ea38d1538ebc750a6ad63afeaf494d7a81422902
Nogadacoding <azxca1731@gamil.com>
2018-06-27 10:49:04 +0900

    monitor 절반 완성

--- routes/monitor.js
+++ routes/monitor.js
@@ -9,10 +9,25 @@ var conn = mysql.createConnection({
   password: 'mju12345',
   database: 'medic'
 });
+conn.connect();
+
 /* GET home page. */
+
+/*
+  먼저 CON에서 쿼리문을 따와서 JSON객체를 만들고(완료)
+  JSON객체를 다른 URL로 해서 주고(완료)
+  AJAX로 값을 받음(완료)
+  그 URL을 주기적으로 참고하여 TABLE을 그림(https://datatables.net/examples/data_sources/ajax.html)
+  그리고 주기적으로 다시 그려줌(AJAX TIMEOUT - 2초:미완료-http://link2me.tistory.com/1139)
+*/
 router.get('/', catchErrors(async (req, res, next) => {
+  res.render('monitor/monitor2');
+}));
+
+//---------------------JSON객체 전달-------------------------//
+router.get('/json', catchErrors(async (req, res, next) => {
+  //환자상세정보 디비에서 가져옴
   var personList = [];
-  conn.connect();
   conn.query('SELECT * FROM patient', function (err, rows, fields) {
     var person;
     if (err)
@@ -28,11 +43,9 @@ router.get('/', catchErrors(async (req, res, next) => {
         }
         personList.push(person);
       }
-      console.log('data is setting');
-      res.render('patientmanagement/monitor2', { "personList": personList });
+      res.json(personList);
     }
   });
-  conn.end();
 }));
 
 module.exports = router;
