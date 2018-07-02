03779eeebf34d51ec79e50cd1e95a5bd85178ebe
Nogadacoding <azxca1731@gamil.com>
2018-07-03 02:15:00 +0900

    이제 과 선택만 남음

--- routes/diagnosis.js
+++ routes/diagnosis.js
@@ -10,10 +10,37 @@ var conn = mysql.createConnection({
     database: 'medic'
 });
 conn.connect();
+var queuelist = [];
+function moveByid(id, action) {
+    for (var i = 0; i < queuelist.length; i++) {
+        if (queuelist[i].patient_id == id) {
+            var x = queuelist.splice(i, 1);
+            console.log(x);
+            var person = {
+                name: x[0].name,
+                patient_id: x[0].patient_id,
+                personal_number: x[0].personal_number,
+                phone_number: x[0].phone_number
+            }
+            queuelist.splice(i + action, 0, person);
+            console.log(queuelist);
+            return;
+        }
+    }
+}
+
+function findByid(id) {
+    for (var i = 0; i < queuelist.length; i++) {
+        if (queuelist[i].patient_id == id) {
+            queuelist.splice(i, 1);
+        }
+    }
+}
 
 router.get('/', catchErrors(async (req, res, next) => {
     //환자상세정보 디비에서 가져옴
     var patientList = [];
+    //console.log(queuelist);
     if (req.query.name) {
         var Sql = "SELECT * FROM patient WHERE name ='" + req.query.name + "'";
     } else if (req.query.patient_id) {
@@ -36,7 +63,7 @@ router.get('/', catchErrors(async (req, res, next) => {
                 }
                 patientList.push(person);
             }
-            res.render('diagnosis/receiptmain', { patientList: patientList });
+            res.render('diagnosis/receiptmain', { queue_number: queuelist.length, queuelist: queuelist.slice(0.5), patientList: patientList });
         }
     });
 }));
@@ -76,20 +103,55 @@ router.get('/', catchErrors(async (req, res, next) => {
 router.get('/queue/:patient_id', catchErrors(async (req, res, next) => {
     //여기서 patient_id얻구 리다이렉트처리
     console.log("queue에다 넣음");
+    var requestPatient = req.params.patient_id;
+    console.log(requestPatient);
+    var insertSql = 'SELECT * FROM patient WHERE patient_id=' + requestPatient;
+    await conn.query(insertSql, function (err, rows, fields) {
+        if (err)
+            console.log('Error while performing Query.', err);
+        else {
+            for (var i = 0; i < rows.length; i++) {
+                var person = {
+                    'name': rows[i].name,
+                    'patient_id': rows[i].patient_id,
+                    'personal_number': rows[i].personal_number,
+                    'phone_number': rows[i].phone_number
+                }
+                queuelist.push(person);
+            }
+        }
+    });
     res.redirect('/diagnosis');
 }));
 //환자 접수 취소함
 router.get('/dequeue/:patient_id', catchErrors(async (req, res, next) => {
     //여기서 patient_id얻구 리다이렉트처리
     console.log("dequeue에다 넣음");
+    var requestPatient = req.params.patient_id;
+    findByid(requestPatient);
     res.redirect('/diagnosis');
 }));
 
-//순서를 바꿀때 하는 작업입니다.
-router.post('/', catchErrors(async (req, res, next) => {
-    var action = req.body.action;
-    var queue_number = req.body.number;
-    console.log(action + queue_number);
+router.get('/next/:patient_id', catchErrors(async (req, res, next) => {
+    //여기서 patient_id얻구 리다이렉트처리
+    console.log("next 넣음");
+    var requestPatient = req.params.patient_id;
+    if (requestPatient != 0) {
+        moveByid(requestPatient, -1);
+    }
+    console.log(queuelist);
+    res.redirect('/diagnosis');
+}));
+
+router.get('/prev/:patient_id', catchErrors(async (req, res, next) => {
+    //여기서 patient_id얻구 리다이렉트처리
+    console.log("prev 넣음");
+    var requestPatient = req.params.patient_id;
+    if (requestPatient != queuelist.length) {
+        moveByid(requestPatient, 1);
+    }
+    console.log(queuelist);
+    res.redirect('/diagnosis');
 }));
 
 module.exports = router;
