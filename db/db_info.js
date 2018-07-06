43c00f14d79682302a8e3d555516852b260c3940
Nogadacoding <azxca1731@gamil.com>
2018-07-06 16:04:42 +0900

    로그인 구현 confict 제거 navbar 수정

---
+++ db/db_info.js
@@ -0,0 +1,11 @@
+module.exports = (function () {
+    return {
+        web: { // real server db info
+            host: '58.123.136.107',
+            port: '3308',
+            user: 'web',
+            password: 'mju12345',
+            database: 'medic'
+        }
+    }
+})();
---
+++ public/js/common.js
@@ -0,0 +1,19 @@
+$.ajax({
+    url: '',
+    type: 'get',
+    success: function (data, textStatus, jqXHR) {
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+    }
+});
+
+
+$.ajax({
+    url: '',
+    type: 'post',
+    data: _data,
+    success: function (data, textStatus, jqXHR) {
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+    }
+});
